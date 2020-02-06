4c93bf51e560efc72fb3b536fcf25e8a6835d51a
Mizo10 <moutazabdelhamid@gmail.com>
2020-02-05 20:28:07 -0500

    Updates

---
+++ precache-manifest.e0e859f47843355aca997eebec64ed51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84eac63a0acb2caafbe7877e95cc375c",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "17ed066df0a6962fead8",
+    "url": "/clicky-game/static/css/main.759602cb.chunk.css"
+  },
+  {
+    "revision": "92e68c3a1cfe74c9d40e",
+    "url": "/clicky-game/static/js/2.a1228723.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/clicky-game/static/js/2.a1228723.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17ed066df0a6962fead8",
+    "url": "/clicky-game/static/js/main.94efe677.chunk.js"
+  },
+  {
+    "revision": "a444cfe878aa3a515eec",
+    "url": "/clicky-game/static/js/runtime-main.24c47d54.js"
+  }
+]);
